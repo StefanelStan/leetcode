f9e07e61b09c552af571f1591285b2ee584804d7
Stefanel Stan <stefanel.stan@yahoo.com>
2021-01-03 21:29:58 +0000

    Leetcode Jan 03

---
+++ src/main/java/com/ss/leetcode/LargestNumber.java
@@ -0,0 +1,59 @@
+package com.ss.leetcode;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class LargestNumber {
+    // https://leetcode.com/problems/largest-number/
+    public String largestNumber(int[] nums) {
+        NavigableMap<Integer, List<String>> sortedNumbers = putNumbersIntoSortedMap(nums);
+        StringBuilder stb = new StringBuilder();
+        for (int i = 9; i >= 1; i--) {
+            if (sortedNumbers.containsKey(i)) {
+                List<String> nrs = sortedNumbers.get(i);
+                nrs.sort(new IntStringComparator());
+                if (!nrs.isEmpty()) {
+                    nrs.forEach(stb::append);
+                }
+            }
+        }
+        if (stb.length() > 0){
+            List<String> nrs = sortedNumbers.get(0);
+            if (nrs != null) {
+                nrs.forEach(stb::append);
+            }
+        } else if (sortedNumbers.containsKey(0)) {
+            stb.append(0);
+        }
+
+        return stb.toString();
+    }
+
+    private NavigableMap<Integer, List<String>> putNumbersIntoSortedMap(int[] nums) {
+        NavigableMap<Integer, List<String>> numbers = new TreeMap<>();
+        for (int num : nums) {
+            String numAsString = String.valueOf(num);
+            int firstDigit = Integer.parseInt(String.valueOf(numAsString.charAt(0)));
+            List<String> set = numbers.get(firstDigit);
+            if (set == null) {
+                set = new LinkedList<>();
+            }
+            set.add(numAsString);
+            numbers.putIfAbsent(firstDigit, set);
+        }
+        return numbers;
+    }
+
+    private static class IntStringComparator implements Comparator<String> {
+
+        @Override
+        public int compare(String string1, String string2) {
+            Long s1s2 = Long.parseLong(string1 + string2);
+            Long s2s1 = Long.parseLong(string2 + string1);
+            return s2s1.compareTo(s1s2);
+        }
+    }
+}
--- src/main/java/com/ss/leetcode/Start.java
+++ src/main/java/com/ss/leetcode/Start.java
@@ -2,6 +2,8 @@ package com.ss.leetcode;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Start {
     public static void main(String[] args) {
@@ -26,7 +28,9 @@ public class Start {
 //        start.bestBreak();
 //        start.longestStringChain();
 //        start.constructKPalindrome(); //unfinished
-        start.excelSheetColumnNumber();
+//        start.excelSheetColumnNumber();
+//        start.largestNumber();
+        start.addToArrayForm();
     }
 
 
@@ -57,22 +61,22 @@ public class Start {
         int[] nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
         int[] nums2 = new int[] { 1, 1, 1 };
         int[] nums3 = new int[] { 1, 1, 2 };
-        int[] nums4 = new int[] { 0,0,1,1,2,3,4 };
+        int[] nums4 = new int[] { 0, 0, 1, 1, 2, 3, 4 };
         RemoveDuplicatesFromSortedArray rdsa = new RemoveDuplicatesFromSortedArray();
         System.out.println("RemoveElement  of " + Arrays.toString(nums) + " = " + rdsa.removeDuplicates(nums) + " =>" + Arrays.toString(nums));
 
-        System.out.println("RemoveElement  of " + Arrays.toString(nums2) + " = " + rdsa.removeDuplicates(nums2)+ " =>" + Arrays.toString(nums2));
+        System.out.println("RemoveElement  of " + Arrays.toString(nums2) + " = " + rdsa.removeDuplicates(nums2) + " =>" + Arrays.toString(nums2));
 
-        System.out.println("RemoveElement  of " + Arrays.toString(nums3) + " = " + rdsa.removeDuplicates(nums3)+ " =>" + Arrays.toString(nums3));
+        System.out.println("RemoveElement  of " + Arrays.toString(nums3) + " = " + rdsa.removeDuplicates(nums3) + " =>" + Arrays.toString(nums3));
 
-        System.out.println("RemoveElement  of " + Arrays.toString(nums4) + " = " + rdsa.removeDuplicates(nums4)+ " =>" + Arrays.toString(nums4));
+        System.out.println("RemoveElement  of " + Arrays.toString(nums4) + " = " + rdsa.removeDuplicates(nums4) + " =>" + Arrays.toString(nums4));
     }
 
     private void handOfStraights() {
         HandOfStraights hos = new HandOfStraights();
-        int[] nums = new int[] {1,2,3,6,2,3,4,7,8};
-        int[] nums2 = new int[] {1,2,3,4,5};
-        int[] nums3 = new int[] {8,10,12};
+        int[] nums = new int[] { 1, 2, 3, 6, 2, 3, 4, 7, 8 };
+        int[] nums2 = new int[] { 1, 2, 3, 4, 5 };
+        int[] nums3 = new int[] { 8, 10, 12 };
         System.out.println("HoS should be true on " + Arrays.toString(nums) + " : " + hos.isNStraightHand(nums, 3));
         System.out.println("HoS should be false on " + Arrays.toString(nums2) + " : " + hos.isNStraightHand(nums2, 4));
         System.out.println("HoS should be false on " + Arrays.toString(nums3) + " : " + hos.isNStraightHand(nums3, 3));
@@ -119,31 +123,31 @@ public class Start {
 
     private void riverRecords() {
         RiverRecords rr = new RiverRecords();
-        List<Integer> readings = Arrays.asList(5,3,6,7,4);
+        List<Integer> readings = Arrays.asList(5, 3, 6, 7, 4);
         List<Integer> readings2 = Arrays.asList(2, 3, 10, 2, 4, 8, 1);
         System.out.println(rr.maxTrailing(readings));
         System.out.println(rr.maxTrailing(readings2));
     }
 
     private void mergeSortList() {
-        List<Integer> list1 =  Arrays.asList(2,4,5,9,9);
-        List<Integer> list2 =  Arrays.asList(0,1,2,3,4);
+        List<Integer> list1 = Arrays.asList(2, 4, 5, 9, 9);
+        List<Integer> list2 = Arrays.asList(0, 1, 2, 3, 4);
         MergeSortList msl = new MergeSortList();
         System.out.println("Merging 2 lists = " + msl.mergeArrays(list1, list2));
     }
 
     private void filterRestaurants() {
-        int[][] restaurants = {{1,4,1,40,10}, {2,8,0,50,5}, {3,8,1,30,4}, {4,10,0,10,3}, {5,1,1,15,1}};
-        int[][] restaurants2 = {{1,4,1,40,10}, {2,8,0,50,5}, {3,8,1,30,4}, {4,10,0,10,3}, {5,1,1,15,1}};
+        int[][] restaurants = { { 1, 4, 1, 40, 10 }, { 2, 8, 0, 50, 5 }, { 3, 8, 1, 30, 4 }, { 4, 10, 0, 10, 3 }, { 5, 1, 1, 15, 1 } };
+        int[][] restaurants2 = { { 1, 4, 1, 40, 10 }, { 2, 8, 0, 50, 5 }, { 3, 8, 1, 30, 4 }, { 4, 10, 0, 10, 3 }, { 5, 1, 1, 15, 1 } };
         FilterRestaurants fr = new FilterRestaurants();
-        System.out.println("Filtered restaurants :" + fr.filterRestaurants(restaurants, 1,50, 10));
+        System.out.println("Filtered restaurants :" + fr.filterRestaurants(restaurants, 1, 50, 10));
     }
 
     private void longestMountain() {
-        int[] numbers = {2,1,4,7,3,2,5};
-        int[] numbers2 = {2,2,2};
-        int[] numbers3 = {0,1,2,3,4,5,4,3,2,1,0};
-        int[] numbers4 = {2,1,4,7,3,2,5};
+        int[] numbers = { 2, 1, 4, 7, 3, 2, 5 };
+        int[] numbers2 = { 2, 2, 2 };
+        int[] numbers3 = { 0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0 };
+        int[] numbers4 = { 2, 1, 4, 7, 3, 2, 5 };
         LongestMountain lm = new LongestMountain();
         System.out.println("Longest mountain 5 = " + lm.longestMountain(numbers));
         System.out.println("Longest mountain 0 = " + lm.longestMountain(numbers2));
@@ -159,32 +163,32 @@ public class Start {
     }
 
     private void removeDuplicates2() {
-        int[] nums = new int[] { 1,1,1,2,2,3};
-        int[] nums2 = new int[] { 0,0,1,1,1,1,2,3,3 };
-        int[] nums3 = new int[] {1,1,1,2,2,2,3,3};
+        int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
+        int[] nums2 = new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+        int[] nums3 = new int[] { 1, 1, 1, 2, 2, 2, 3, 3 };
         RemoveDuplicatesFromSortedArray2 rdsa = new RemoveDuplicatesFromSortedArray2();
 //        System.out.println("RemoveElement  of " + Arrays.toString(nums) + " = " + rdsa.removeDuplicates(nums) + " =>" + Arrays.toString(nums));
 
 //        System.out.println("RemoveElement  of " + Arrays.toString(nums2) + " = " + rdsa.removeDuplicates(nums2)+ " =>" + Arrays.toString(nums2));
-        System.out.println("RemoveElement  of " + Arrays.toString(nums3) + " = " + rdsa.removeDuplicates(nums3)+ " =>" + Arrays.toString(nums3));
+        System.out.println("RemoveElement  of " + Arrays.toString(nums3) + " = " + rdsa.removeDuplicates(nums3) + " =>" + Arrays.toString(nums3));
     }
 
     private void minimumDeletionCost() {
         MinimumDeletionCost mdc = new MinimumDeletionCost();
-        System.out.println("Min cost deletion of[abaac] = " + mdc.minCost("abaac", new int[]{1,2,3,4,5}));
-        System.out.println("Min cost deletion of[abc] = " + mdc.minCost("abc", new int[]{1,2,3}));
-        System.out.println("Min cost deletion of[aabaa] = " + mdc.minCost("aabaa", new int[]{1,2,3,4,1}));
-        System.out.println("Min cost deletion of[bbbaaa] = " + mdc.minCost("bbbaaa", new int[]{4,9,3,8,8,9}));
+        System.out.println("Min cost deletion of[abaac] = " + mdc.minCost("abaac", new int[] { 1, 2, 3, 4, 5 }));
+        System.out.println("Min cost deletion of[abc] = " + mdc.minCost("abc", new int[] { 1, 2, 3 }));
+        System.out.println("Min cost deletion of[aabaa] = " + mdc.minCost("aabaa", new int[] { 1, 2, 3, 4, 1 }));
+        System.out.println("Min cost deletion of[bbbaaa] = " + mdc.minCost("bbbaaa", new int[] { 4, 9, 3, 8, 8, 9 }));
     }
 
     private void largestMultipleOfThree() {
-        int[] numbers = {8,1,9};
-        int[] numbers2 = {8,6,7,1,0};
-        int[] numbers3 = {0,0,0,0,0,0};
-        int[] numbers4 = {9,8,6,8,6};
-        int[] numbers5 = {0};
-        int[] numbers6 = {1};
-        int[] numbers7 = {7};
+        int[] numbers = { 8, 1, 9 };
+        int[] numbers2 = { 8, 6, 7, 1, 0 };
+        int[] numbers3 = { 0, 0, 0, 0, 0, 0 };
+        int[] numbers4 = { 9, 8, 6, 8, 6 };
+        int[] numbers5 = { 0 };
+        int[] numbers6 = { 1 };
+        int[] numbers7 = { 7 };
         LargestMultipleOfThree lmot = new LargestMultipleOfThree();
 //        System.out.println("Largest M3 from " + Arrays.toString(numbers) + " [981= " + lmot.largestMultipleOfThree(numbers));
 //        System.out.println("Largest M3 from " + Arrays.toString(numbers2) + " [8760 = " + lmot.largestMultipleOfThree(numbers2));
@@ -209,8 +213,8 @@ public class Start {
 
     private void longestStringChain() {
         LongestStringChain lsc = new LongestStringChain();
-        String[] words = {"a","b","ba","bca","bda","bdca"};
-        String[] words2 = {"xbc","pcxbcf","xb","cxbc","pcxbc"};
+        String[] words = { "a", "b", "ba", "bca", "bda", "bdca" };
+        String[] words2 = { "xbc", "pcxbcf", "xb", "cxbc", "pcxbc" };
         System.out.println("4 ==" + lsc.longestStrChain(words));
         System.out.println("5 ==" + lsc.longestStrChain(words2));
 
@@ -232,5 +236,30 @@ public class Start {
         System.out.println("28 ==" + escn.titleToNumber("AB"));
         System.out.println("701 ==" + escn.titleToNumber("ZY"));
     }
+
+    private void largestNumber() {
+        LargestNumber ln = new LargestNumber();
+        int[] nums = { 10, 2 };
+        int[] nums2 = { 3, 30, 34, 5, 9 };
+        int[] nums3 = { 3, 30, 34, 5, 9,  9125, 90, 925};
+        int[] nums4 = { 432, 43243};
+        int[] nums5 = {8308,8308,830};
+//        System.out.println("210 ==" + ln.largestNumber(nums));
+//        System.out.println("9534330 ==" + ln.largestNumber(nums2));
+//        System.out.println("9925912590 ==" + ln.largestNumber(nums3));
+//        System.out.println("43243432 ==" + ln.largestNumber(nums4));
+        System.out.println("83088308830 == " + ln.largestNumber(nums5));
+    }
+
+    private void addToArrayForm() {
+        AddToArrayForm ataf = new AddToArrayForm();
+        int[] digits1 = {1,2,0,0};
+        int[] digits2 = {2,7,4};
+        int[] digits3 = {9,9,9,9,9,9,9,9,9,9};
+        System.out.println(ataf.addToArrayForm(digits1, 34));
+        System.out.println(ataf.addToArrayForm(digits2, 181));
+        System.out.println(ataf.addToArrayForm(digits3, 1));
+    }
+
 }
 
