d7fc91556e53b3ae50626eb4a952856dfc17d3b2
Stefanel Stan <stefanel.stan@yahoo.com>
2021-10-03 09:50:51 +0300

    LeetCode Oct 03 - Weekly 261

--- src/main/java/com/ss/leetcode/StartOctober.java
+++ src/main/java/com/ss/leetcode/StartOctober.java
@@ -1,10 +1,15 @@
 package com.ss.leetcode;
 
+import com.ss.leetcode.august.StoneGame;
 import com.ss.leetcode.october.AddDigits;
 import com.ss.leetcode.october.Convert1DArrayInto2DArray;
+import com.ss.leetcode.october.FindMissingObservations;
+import com.ss.leetcode.october.JumpGame;
 import com.ss.leetcode.october.MaximumNumberOfWaysToPartitionAnArray;
+import com.ss.leetcode.october.MinimumMovesToConvertString;
 import com.ss.leetcode.october.NumberOfPairsConcatenationTarget;
 import com.ss.leetcode.october.ReversePrefixOfWord;
+import com.ss.leetcode.october.StoneGameIX;
 
 import java.util.Arrays;
 
@@ -16,7 +21,11 @@ public class StartOctober {
 //        start.addDigits();
 //        start.convert1DArrayInto2DArray();
 //        start.numberOfPairsConcatenationTarget();
-        start.maximumNumberOfWaysToPartitionAnArray();
+//        start.maximumNumberOfWaysToPartitionAnArray();
+//        start.minimumMovesToConvertString();
+//        start.findMissingObservations();
+//        start.stoneGameIX();
+        start.jumpGame();
     }
 
     public void reversePrefixOfWord() {
@@ -61,4 +70,43 @@ public class StartOctober {
     }
     // 5873. Maximize the Confusion of an Exam
 
+    public void minimumMovesToConvertString() {
+        MinimumMovesToConvertString mmtcs = new MinimumMovesToConvertString();
+
+        System.out.println("1 == " + mmtcs.minimumMoves("XXX"));
+        System.out.println("2 == " + mmtcs.minimumMoves("XX0X"));
+        System.out.println("0 == " + mmtcs.minimumMoves("0000"));
+        System.out.println("1 == " + mmtcs.minimumMoves("00X"));
+        System.out.println("1 == " + mmtcs.minimumMoves("00XX"));
+    }
+
+    public void findMissingObservations() {
+        FindMissingObservations fmo = new FindMissingObservations();
+
+        System.out.println("[6,6] == " + Arrays.toString(fmo.missingRolls(new int[]{3,2,4,3}, 4, 2)));
+        System.out.println("[2,3,2,2] == " + Arrays.toString(fmo.missingRolls(new int[]{1,5,6}, 3, 4)));
+        System.out.println("[] == " + Arrays.toString(fmo.missingRolls(new int[]{1,2,3,4}, 6, 4)));
+        System.out.println("[5] == " + Arrays.toString(fmo.missingRolls(new int[]{1}, 3, 1)));
+        System.out.println("[6,6,5,2] == " + Arrays.toString(fmo.missingRolls(new int[]{3,5,1,6,4,2}, 4, 4)));
+        System.out.println("[] == " + Arrays.toString(fmo.missingRolls(new int[]{6,3,4,3,5,3}, 1, 6)));
+        System.out.println("[] == " + Arrays.toString(fmo.missingRolls(new int[]{4,2,2,5,4,5,4,5,3,3,6,1,2,4,2,1,6,5,4,2,3,4,2,3,3,5,4,1,4,4,5,3,6,1,5,2,3,3,6,1,6,4,1,3}, 2, 53)));
+    }
+
+    public void stoneGameIX() {
+        StoneGameIX stix = new StoneGameIX();
+
+//        System.out.println("true == " + stix.stoneGameIX(new int[]{2,1}));
+//        System.out.println("false == " + stix.stoneGameIX(new int[]{2}));
+//        System.out.println("false == " + stix.stoneGameIX(new int[]{5,1,2,4,3}));
+        System.out.println("true == " + stix.stoneGameIX(new int[]{1,11,12,17,6}));
+    }
+
+    public void jumpGame() {
+        JumpGame jg = new JumpGame();
+
+        System.out.println("true == " + jg.canJump(new int[]{2,3,1,1,4}));
+        System.out.println("false == " + jg.canJump(new int[]{3,2,1,0,4}));
+        System.out.println("true == " + jg.canJump(new int[]{0}));
+        System.out.println("true == " + jg.canJump(new int[]{2,0,0}));
+    }
 }
