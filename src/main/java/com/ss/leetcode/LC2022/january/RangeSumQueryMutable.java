0e6a42fdd1325b1e458e1f7458f6bba5085ac352
Stefanel Stan <stefanel.stan@yahoo.com>
2022-07-31 23:30:12 +0100

    LeetCode Jul 31

--- src/main/java/com/ss/leetcode/LC2022/january/RangeSumQueryMutable.java
+++ src/main/java/com/ss/leetcode/LC2022/january/RangeSumQueryMutable.java
@@ -2,6 +2,33 @@ package com.ss.leetcode.LC2022.january;
 
 public class RangeSumQueryMutable {
     // https://leetcode.com/problems/range-sum-query-mutable/
+    // TLE on 12/15
+    int[] sums;
+    int[] numz;
+    public RangeSumQueryMutable(int[] nums) {
+        this.numz = nums;
+        sums = new int[nums.length];
+        calculateSum(0);
+    }
+
+    public void update(int index, int val) {
+        numz[index] = val;
+        calculateSum(index);
+    }
+
+    public int sumRange(int left, int right) {
+        int sumLeft = left == 0 ? 0 : sums[left -1];
+        return sums[right] - sumLeft;
+    }
+
+    private void calculateSum(int from) {
+        sums[from] = from == 0 ? numz[0] : sums[from-1] + numz[from];
+        for (int i = from + 1; i < numz.length; i++) {
+            sums[i] = sums[i-1] + numz[i];
+        }
+    }
+
+    /* Wrong way, too slow and inefficient
     int[] nums;
     int[] leftSum;
     int[] rightSum;
@@ -31,4 +58,5 @@ public class RangeSumQueryMutable {
             rightSum[j] = rightSum[j+1] + nums[j];
         }
     }
+     */
 }
