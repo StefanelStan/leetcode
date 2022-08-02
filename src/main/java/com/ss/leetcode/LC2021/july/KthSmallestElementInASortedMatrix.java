07806e2d006ae9b51baf296acdca93154814cfb3
Stefanel Stan <stefanel.stan@yahoo.com>
2022-08-02 22:31:17 +0100

    LeetCode Aug 02

--- src/main/java/com/ss/leetcode/LC2021/july/KthSmallestElementInASortedMatrix.java
+++ src/main/java/com/ss/leetcode/LC2021/july/KthSmallestElementInASortedMatrix.java
@@ -1,9 +1,26 @@
 package com.ss.leetcode.LC2021.july;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthSmallestElementInASortedMatrix {
+    // https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
     public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length * matrix.length;
+        int maxSize = n - k + 1;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int[] row : matrix) {
+            for (int element : row) {
+                queue.add(element);
+                if (queue.size() > maxSize) {
+                    queue.poll();
+                }
+            }
+        }
+        return queue.poll();
+    }
+
+    public int kthSmallest2(int[][] matrix, int k) {
         int[] newArray =  new int[matrix.length * matrix[0].length];
         int startPosition = 0;
         for (int[] line : matrix) {
