9144576c481714617a5c55104204b0b615937292
Stefanel Stan <stefanel.stan@yahoo.com>
2022-08-07 21:50:28 +0100

    LeetCode Aug 07

--- src/main/java/com/ss/leetcode/LC2022/StartAugust.java
+++ src/main/java/com/ss/leetcode/LC2022/StartAugust.java
@@ -1,12 +1,14 @@
 package com.ss.leetcode.LC2022;
 
+import com.ss.leetcode.LC2022.august.CountVowelsPermutation;
 import com.ss.leetcode.LC2022.august.MakeArrayZeroBySubtractingEqualAmounts;
 
 public class StartAugust {
     public static void main(String[] args) {
         StartAugust start = new StartAugust();
 
-        start.makeArrayZeroBySubtractingEqualAmounts();
+//        start.makeArrayZeroBySubtractingEqualAmounts();
+        start.countVowelsPermutation();
     }
 
     public void makeArrayZeroBySubtractingEqualAmounts() {
@@ -15,5 +17,16 @@ public class StartAugust {
         System.out.println("3 == " + mazbsea.minimumOperations(new int[]{1,5,0,3,5}));
         System.out.println("0 == " + mazbsea.minimumOperations(new int[]{0}));
         System.out.println("5 == " + mazbsea.minimumOperations(new int[]{7,1,5,0,3,5,6}));
+    }
+
+    public void countVowelsPermutation() {
+        CountVowelsPermutation cvp = new CountVowelsPermutation();
+
+        System.out.println("5 == " + cvp.countVowelPermutation(1));
+        System.out.println("10 == " + cvp.countVowelPermutation(2));
+        System.out.println("68 == " + cvp.countVowelPermutation(5));
+        System.out.println("759959057 == " + cvp.countVowelPermutation(20000));
+        System.out.println("377163667 == " + cvp.countVowelPermutation(15682));
+        System.out.println("288864303 == " + cvp.countVowelPermutation(4821));
     }
 }
---
+++ src/main/java/com/ss/leetcode/LC2022/august/CountVowelsPermutation.java
@@ -0,0 +1,56 @@
+package com.ss.leetcode.LC2022.august;
+
+import java.util.Arrays;
+
+public class CountVowelsPermutation {
+    // https://leetcode.com/problems/count-vowels-permutation/
+    /** Algorithm
+         1. Look at the table of vowels combinations: a can only be followed by e,
+             e may only be followed by a or i.
+             Thus, all the 2-letter words starting with a can only be [ae].
+             All the 2-letter words starting with e can only be [ea] and [ei].
+         2. Looking in advance, all 10-letter words starting with e can only be
+             [ea...8_letters] and [ei.._8_letters].
+             So, all 10-letter words starting with e will have a length equal to the sum
+             of all 9 letters starting with a + all 9 letters starting with i.
+         3. Based on this fact, let's build our permutations from 1 to n.
+         4. Declare a matrix permutations[n][5], each row representing permutations of length i and
+         5 - the number of columns (a,e,i,o,u)
+         5. First row (permutations[0] will have 1 on each column, as you can only have 1 single letter
+         6. Starting from row = 1 to n-1, apply the set of rules on each column
+             (replace a,e,i,o,u with 0,1,2,3,4. Using letters just to point the idea)
+             permutations[row][a] = permutations[row-1][e]
+             permutations[row][e] = permutations[row-1][a] + permutations[row-1][i]
+             ...
+             permutations[row][u] = permutations[row][a]
+         7. The answer if the sum of all permutations of row n-1.
+     */
+    public int countVowelPermutation(int n) {
+        int[][] permutations = new int[n][5];
+        Arrays.fill(permutations[0], 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < 5; j++) {
+                permutations[i][j] = (int)(getPermutationsOf(permutations, i, j) % 1_000_000_007);
+            }
+        }
+        long sum = 0;
+        for (long p : permutations[n-1]) {
+            sum += p;
+        }
+        return (int)(sum % 1_000_000_007);
+    }
+
+    private long getPermutationsOf(int[][] permutations, int i, int j) {
+        long sum = 0;
+        switch (j) {
+            case 0 -> sum = permutations[i - 1][1];
+            case 1 -> sum += (long) permutations[i - 1][0] + permutations[i - 1][2];
+            case 2 -> sum +=
+                (long) permutations[i - 1][0] + permutations[i - 1][1] + permutations[i - 1][3]
+                    + permutations[i - 1][4];
+            case 3 -> sum += (long) permutations[i - 1][2] + permutations[i - 1][4];
+            case 4 -> sum = permutations[i - 1][0];
+        }
+        return sum;
+    }
+}
