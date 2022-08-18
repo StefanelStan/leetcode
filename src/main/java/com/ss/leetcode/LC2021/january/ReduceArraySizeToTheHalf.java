e1d869c07a34c16e65ede603208d44cfdb4fda0b
Stefanel Stan <stefanel.stan@yahoo.com>
2022-08-18 22:05:55 +0100

    LeetCode Aug 18

--- src/main/java/com/ss/leetcode/LC2021/january/ReduceArraySizeToTheHalf.java
+++ src/main/java/com/ss/leetcode/LC2021/january/ReduceArraySizeToTheHalf.java
@@ -1,15 +1,47 @@
 package com.ss.leetcode.LC2021.january;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class ReduceArraySizeToTheHalf {
     // https://leetcode.com/problems/reduce-array-size-to-the-half/
     public int minSetSize(int[] arr) {
+        Map<Integer, Integer> count =  new HashMap<>();
+        for (int num : arr) {
+            count.merge(num, 1, Integer::sum);
+        }
+        PriorityQueue<Integer> highestCount = new PriorityQueue<>(Comparator.reverseOrder());
+        highestCount.addAll(count.values());
+        int setSize = 0, minNums = 0;
+        while(setSize < arr.length / 2) {
+            setSize += highestCount.poll();
+            minNums++;
+        }
+        return minNums;
+    }
+
+    public int minSetSize2(int[] arr) {
+        int[] count = new int[100_001];
+        for (int num : arr) {
+            count[num]++;
+        }
+        Arrays.sort(count);
+        int minSetSize = 0, eliminated = 0, half = arr.length / 2;
+
+        for (int i = count.length -1; eliminated < half && i >= 1; i--) {
+            eliminated += count[i];
+            minSetSize++;
+        }
+        return minSetSize;
+    }
+
+    public int minSetSize3(int[] arr) {
         Map<Integer, Integer> numbers = new HashMap<>();
         for(int number : arr) {
             numbers.merge(number, 1, Integer::sum);
