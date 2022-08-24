eae6975eee44220411bca2bbc6f65de0cf5cc2d8
Stefanel Stan <stefanel.stan@yahoo.com>
2022-08-24 20:28:09 +0100

    LeetCode Aug 24

--- src/main/java/com/ss/leetcode/LC2021/november/PowerOfThree.java
+++ src/main/java/com/ss/leetcode/LC2021/november/PowerOfThree.java
@@ -3,6 +3,13 @@ package com.ss.leetcode.LC2021.november;
 public class PowerOfThree {
     // https://leetcode.com/problems/power-of-three/
     public boolean isPowerOfThree(int n) {
+        while(n >= 3 && n % 3 == 0) {
+            n = n / 3;
+        }
+        return n == 1;
+    }
+
+    public boolean isPowerOfThree2(int n) {
         int remainder = 0;
         if (n <= 0) return false;
         while (n > 1 && remainder == 0) {
