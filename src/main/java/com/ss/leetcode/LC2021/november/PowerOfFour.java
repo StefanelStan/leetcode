823719353ae7cca67b295332c774c01f4b0c0f09
Stefanel Stan <stefanel.stan@yahoo.com>
2022-08-25 21:51:47 +0100

    LeetCode Aug 25

--- src/main/java/com/ss/leetcode/LC2021/november/PowerOfFour.java
+++ src/main/java/com/ss/leetcode/LC2021/november/PowerOfFour.java
@@ -5,8 +5,15 @@ import java.util.Set;
 
 public class PowerOfFour {
     // https://leetcode.com/problems/power-of-four/
-    Set<Integer> fourPower;
     public boolean isPowerOfFour(int n) {
+        while (n >= 4 && n % 4 == 0) {
+            n /= 4;
+        }
+        return n == 1;
+    }
+
+    Set<Integer> fourPower;
+    public boolean isPowerOfFour2(int n) {
         setFourPower();
         return fourPower.contains(n);
     }
