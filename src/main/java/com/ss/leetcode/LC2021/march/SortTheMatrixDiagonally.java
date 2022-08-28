849a1f8e08bfe4e2d718448a3c654dea32c08104
Stefanel Stan <stefanel.stan@yahoo.com>
2022-08-28 16:40:29 +0100

    LeetCode Aug 28

--- src/main/java/com/ss/leetcode/LC2021/march/SortTheMatrixDiagonally.java
+++ src/main/java/com/ss/leetcode/LC2021/march/SortTheMatrixDiagonally.java
@@ -5,6 +5,32 @@ import java.util.Arrays;
 public class SortTheMatrixDiagonally {
     // https://leetcode.com/problems/sort-the-matrix-diagonally/
     public int[][] diagonalSort(int[][] mat) {
+        if (mat.length == 1 || mat[0].length == 1) {
+            return mat;
+        }
+        int[][] sorted = new int[mat.length][mat[0].length];
+        for (int i = 0; i < mat.length; i++) {
+            sortAndInsert(mat, sorted, i, 0);
+        }
+        for (int j = 1; j < mat[0].length; j++) {
+            sortAndInsert(mat, sorted, 0, j);
+        }
+        return sorted;
+    }
+
+    private void sortAndInsert(int[][] mat, int[][] sorted, int row, int col) {
+        int[] nrs = new int[Math.min(mat.length - row, mat[0].length - col)];
+        for(int i = 0; i < nrs.length; i++) {
+            nrs[i] = mat[row + i][col + i];
+        }
+        Arrays.sort(nrs);
+        for (int i = 0; i < nrs.length; i++) {
+            sorted[row + i][col + i] =  nrs[i];
+        }
+    }
+
+
+    public int[][] diagonalSort2(int[][] mat) {
         // you have to calculate for column n to col 1 ;
         // second for to go from line 1 to line n
         for (int j = mat[0].length - 1; j >= 0; j--) {
