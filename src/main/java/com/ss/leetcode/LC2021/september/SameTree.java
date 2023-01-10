3248db375aa38b61d5ebd27279fe5d9eb19e7f78
Stefanel Stan <stefanel.stan@yahoo.com>
2023-01-10 20:35:04 +0000

    LeetCode Jan 10

--- src/main/java/com/ss/leetcode/LC2021/september/SameTree.java
+++ src/main/java/com/ss/leetcode/LC2021/september/SameTree.java
@@ -5,11 +5,19 @@ import com.ss.leetcode.shared.TreeNode;
 public class SameTree {
     // https://leetcode.com/problems/same-tree/
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+        if (p == q) {
+            return true;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
         boolean[] isSameTree = {true};
         isSameTree(p,q, isSameTree);
         return isSameTree[0];
     }
-
     private void isSameTree(TreeNode p, TreeNode q, boolean[] isSameTree) {
         if (isSameTree[0]) {
             if ((p == null && q != null) || (p != null && q == null)) {
