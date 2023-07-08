384f23dcd39c0b04006245daff64b08ce5d98115
Stefanel Stan <stefanel.stan@yahoo.com>
2023-07-08 20:21:10 +0100

    LeetCode Jul 08 (Maximum Tiles Covered by a Carpet)

--- src/main/java/com/ss/leetcode/LC2022/may/MaximumWhiteTilesCoveredByACarpet.java
+++ src/main/java/com/ss/leetcode/LC2022/may/MaximumWhiteTilesCoveredByACarpet.java
@@ -5,7 +5,74 @@ import java.util.Comparator;
 
 public class MaximumWhiteTilesCoveredByACarpet {
     // https://leetcode.com/problems/maximum-white-tiles-covered-by-a-carpet/
+    /** Algorithm
+        1. Use a flexible window - two pointers. Right will point to current tile and left will point to the first tile covered by the carpet
+        2. For each tile segment, cover the current segment. Increase the number of covered tiles AND used carpet.
+        3. If you've used too much carpet (used > carpetLen)
+            - try to uncover as much carpet from the leftmost tile where the carpet starts.
+            - if completely uncovering that segment does not give back enough carpet, then switch to next segment (left++)
+            - when switching to next segment, you also gain back the carpet that covers the empty/blank space between segments
+        4. Also, keep track of where your carpet starts within the left segment:
+            EG: [1,10] but you might have to cover some tiles, thus your carpet can start from position 5.
+        5. At each step, check the max tiles covered by the carpet.
+     */
     public int maximumWhiteTiles(int[][] tiles, int carpetLen) {
+        Arrays.sort(tiles, Comparator.comparingInt(a -> a[0]));
+        FlexibleCarpet flexibleCarpet = new FlexibleCarpet(tiles, carpetLen);
+        int maxCoveredTiles = 0;
+        for (int i = 0; i < tiles.length; i++) {
+            flexibleCarpet.coverNextTile();
+            maxCoveredTiles = Math.max(maxCoveredTiles, flexibleCarpet.getCoveredTiles());
+        }
+        return maxCoveredTiles;
+    }
+
+    private static class FlexibleCarpet {
+        private final int[][] tiles;
+        private final int maxCarpetLength;
+        private int usedCarpet;
+        private int excessCarpet;
+        private int carpetStart, left, coveredTiles, right;
+
+        public FlexibleCarpet(int[][] tiles, int maxCarpetLength) {
+            this.tiles = tiles;
+            this.maxCarpetLength = maxCarpetLength;
+            carpetStart = tiles[0][0];
+        }
+
+        public void coverNextTile() {
+            coveredTiles += tiles[right][1] + 1 - tiles[right][0];
+            usedCarpet += ((tiles[right][1] + 1 - tiles[right][0])  +
+                (right > 0 ? tiles[right][0] - tiles[right - 1][1] - 1 : 0));
+            right++;
+            adjustCarpet();
+        }
+
+        private void adjustCarpet() {
+            while (usedCarpet > maxCarpetLength) {
+                excessCarpet = usedCarpet - maxCarpetLength;
+                if (excessCarpet >= tiles[left][1] + 1 - carpetStart) {
+                    usedCarpet -= ((tiles[left][1] + 1 - carpetStart) + (tiles[left + 1][0] - tiles[left][1] - 1));
+                    coveredTiles -= (tiles[left][1] + 1 - carpetStart);
+                    left++;
+                    carpetStart = tiles[left][0];
+
+                } else {
+                    carpetStart += excessCarpet;
+                    usedCarpet -= excessCarpet;
+                    coveredTiles -= excessCarpet;
+                }
+            }
+        }
+
+        public int getCoveredTiles() {
+            return coveredTiles;
+        }
+    }
+
+
+    // ALL WRONG
+    public int maximumWhiteTiles2(int[][] tiles, int carpetLen) {
         if (carpetLen == 1) {
             return carpetLen;
         } else {
