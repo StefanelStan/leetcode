e9216a12df1ad56e6fcebb1a3c4ebeb42aa1bd1d
Stefanel Stan <stefanel.stan@yahoo.com>
2023-07-14 00:15:47 +0100

    LeetCode Jul 13 (Kahn Topological Sort - Clean Queue Version)

--- src/main/java/com/ss/leetcode/LC2023/april/CourseSchedule.java
+++ src/main/java/com/ss/leetcode/LC2023/april/CourseSchedule.java
@@ -1,12 +1,62 @@
 package com.ss.leetcode.LC2023.april;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class CourseSchedule {
     // https://leetcode.com/problems/course-schedule
-    // Kahn's Topological Sort
+    // Kahn topological sort with a clean queue of nodes
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
+        List<Integer>[] outgoing = new List[numCourses];
+        generateGraph(prerequisites, inDegree, outgoing);
+        int totalCourses = countDoableCourses(inDegree, outgoing);
+        return totalCourses == numCourses;
+    }
+
+    private int countDoableCourses(int[] inDegree, List<Integer>[] outgoing) {
+        Queue<Integer> doneCourses = getInitialCourses(inDegree);
+        int totalCourses = 0, currentCourse;
+        while(!doneCourses.isEmpty()) {
+            currentCourse = doneCourses.poll();
+            totalCourses++;
+            if (outgoing[currentCourse] != null) {
+                for (int nextCourse : outgoing[currentCourse]) {
+                    inDegree[nextCourse]--;
+                    if (inDegree[nextCourse] == 0) {
+                        doneCourses.offer(nextCourse);
+                    }
+                }
+            }
+        }
+        return totalCourses;
+    }
+
+    private void generateGraph(int[][] prerequisites, int[] inDegree, List<Integer>[] outgoing) {
+        for (int[] req : prerequisites) {
+            inDegree[req[1]]++;
+            if (outgoing[req[0]] == null) {
+                outgoing[req[0]] = new ArrayList<>();
+            }
+            outgoing[req[0]].add(req[1]);
+        }
+    }
+
+    private Queue<Integer> getInitialCourses(int[] inDegree) {
+        Queue<Integer> doneCourses = new LinkedList<>();
+        for (int i = 0; i < inDegree.length; i++) {
+            if (inDegree[i] == 0) {
+                doneCourses.offer(i);
+            }
+        }
+        return doneCourses;
+    }
+
+
+    // Kahn's Topological Sort
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
         List<Integer>[] outgoing = new List[numCourses];
         int[] inDegree = new int[numCourses];
         calculateInDegree(outgoing, inDegree, prerequisites);
