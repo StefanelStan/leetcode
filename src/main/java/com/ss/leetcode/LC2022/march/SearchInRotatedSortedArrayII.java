cf9db32432a9a95d7b39ba8ba71cafd4545c1000
Stefanel Stan <stefanel.stan@yahoo.com>
2023-08-10 23:42:25 +0100

    LeetCode Aug 10 (Search in Rotated Array II - PRO MODE)

--- src/main/java/com/ss/leetcode/LC2022/march/SearchInRotatedSortedArrayII.java
+++ src/main/java/com/ss/leetcode/LC2022/march/SearchInRotatedSortedArrayII.java
@@ -2,7 +2,49 @@ package com.ss.leetcode.LC2022.march;
 
 public class SearchInRotatedSortedArrayII {
     // https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+    // PRO MODE
+    /** Algorithm
+        1. Use the middle/pivot to determine if the left / right sequence is sorted
+        2. If left sequence is STRICTLY sorted
+            - If target is between left/right, search in on the left side
+            - Else target is lesser or higher than the left interval, so check it on the right side
+        3. Else
+            - If right side is STRICTLY sorted and target is between the interval, search it only on the right side
+            - Else we cannot determine if target is on the left or right
+        4. We can have edge cases such as 23222222 or 2223222 or 22222232.
+           Thus, if pivot == left can be misleading or pivot == right can be misleading.
+     */
     public boolean search(int[] nums, int target) {
+        return search(nums, 0, nums.length -1, target);
+    }
+
+    private boolean search(int[] nums, int low, int high, int target) {
+        if (low > high) {
+            return false;
+        } else if (low == high || low + 1 == high) {
+            return nums[low] == target || nums[high] == target;
+        }
+        int pivot = low + (high - low) / 2;
+        boolean leftSorted = nums[low] < nums[pivot];
+        boolean rightSorted = nums[high] > nums[pivot];
+        if (leftSorted) {
+            if (nums[low] <= target && nums[pivot] > target) {
+                return search(nums, low, pivot, target);
+            } else {
+                return search(nums, pivot, high, target);
+            }
+        } else {
+            if (rightSorted && nums[pivot] < target && nums[high] >= target) {
+                return search(nums, pivot, high, target);
+            } else {
+                return search(nums, low, pivot, target) || search(nums, pivot, high, target);
+            }
+        }
+    }
+
+
+    // LOL Solution
+    public boolean search2(int[] nums, int target) {
         if (nums.length < 4) {
             return contains(nums, 0, nums.length -1, target);
         } else {
