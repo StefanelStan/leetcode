e0c920c1874292b4c7cf853b26fd887d3bdd6d1b
Stefanel Stan <stefanel.stan@yahoo.com>
2023-08-21 22:42:45 +0100

    LeetCode Aug 21

--- src/main/java/com/ss/leetcode/LC2021/november/RepeatedSubstringPattern.java
+++ src/main/java/com/ss/leetcode/LC2021/november/RepeatedSubstringPattern.java
@@ -3,6 +3,28 @@ package com.ss.leetcode.LC2021.november;
 public class RepeatedSubstringPattern {
     // https://leetcode.com/problems/repeated-substring-pattern/
     public boolean repeatedSubstringPattern(String s) {
+        char[] chars = s.toCharArray();
+        boolean canRepeat = false;
+        for (int i = 1; i <= chars.length / 2 && !canRepeat; i++) {
+            if (chars.length % i == 0) {
+                canRepeat = checkIfCanRepeat(chars, i);
+            }
+        }
+        return canRepeat;
+    }
+
+    private boolean checkIfCanRepeat(char[] chars, int stop) {
+        for (int j = stop; j < chars.length; j += stop) {
+            for (int i = 0; i < stop; i++) {
+                if (chars[i] != chars[j + i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean repeatedSubstringPattern2(String s) {
         if (s.length() == 1) {
             return false;
         }
