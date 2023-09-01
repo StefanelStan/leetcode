39e3ea03bdb6d07fada41d6c9bdf905032c9eed9
StefanelStan <stefanel.stan@yahoo.com>
2023-09-01 23:50:21 +0100

    LeetCode Sep 01 (Mastering the System Design Interview)

--- src/main/java/com/ss/leetcode/LC2020/december/CountingBits.java
+++ src/main/java/com/ss/leetcode/LC2020/december/CountingBits.java
@@ -1,12 +1,23 @@
 package com.ss.leetcode.LC2020.december;
 
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class CountingBits {
     // https://leetcode.com/problems/counting-bits/
-    public int[] countBits(int num) {
+    public int[] countBits(int n) {
+        int[] bitCount = new int[n+1];
+        int currentNumber, currentBits;
+        while (n > 0) {
+            currentNumber = n;
+            currentBits = 0;
+            while(currentNumber > 0) {
+                currentBits += (currentNumber & 1);
+                currentNumber = currentNumber >> 1;
+            }
+            bitCount[n--] = currentBits;
+        }
+        return bitCount;
+    }
+
+    public int[] countBits2(int num) {
         int[] bitCounts = new int[num + 1];
         bitCounts[0] = 0;
         for (int i = 1; i <= num; i++) {
