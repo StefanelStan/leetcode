23ae88ba70048127405df7538f24f68010235ee2
StefanelStan <stefanel.stan@yahoo.com>
2023-09-24 22:38:35 +0100

    LeetCode Sep 24 (Champagne Tower Extra Performance)

--- src/main/java/com/ss/leetcode/LC2022/march/ChampagneTower.java
+++ src/main/java/com/ss/leetcode/LC2022/march/ChampagneTower.java
@@ -2,7 +2,31 @@ package com.ss.leetcode.LC2022.march;
 
 public class ChampagneTower {
     // https://leetcode.com/problems/champagne-tower/
+    // MORE PERFORMANT
     public double champagneTower(int poured, int query_row, int query_glass) {
+        double[] current = {poured};
+        double[] next = new double[current.length + 1];
+        int row = 0, col = 0;
+        double halfDiff = 0;
+        for (row = 0; row <= query_row; row++) {
+            for (col = 0; col <= row; col++) {
+                if (current[col] > 1) {
+                    halfDiff = (current[col] - 1) /2;
+                    next[col] += halfDiff;
+                    next[col + 1] += halfDiff;
+                }
+            }
+            if (row == query_row && col > query_glass) {
+                break;
+            }
+            current = next;
+            next = new double[current.length + 1];
+        }
+        return current[query_glass] > 1 ? 1 : current[query_glass];
+    }
+
+
+    public double champagneTower2(int poured, int query_row, int query_glass) {
         double[][] tower = new double[101][201];
         int rowGlass, left = 100, right = 100;
         tower[0][100] = poured;
