af141c4e5af1a1f24e3583f663533df84744d17b
StefanelStan <stefanel.stan@yahoo.com>
2023-10-01 22:19:33 +0300

    LeetCode Oct 01

--- src/main/java/com/ss/leetcode/LC2021/august/ReverseWordsInAStringIII.java
+++ src/main/java/com/ss/leetcode/LC2021/august/ReverseWordsInAStringIII.java
@@ -1,7 +1,32 @@
 package com.ss.leetcode.LC2021.august;
 
 public class ReverseWordsInAStringIII {
+    // https://leetcode.com/problems/reverse-words-in-a-string-iii
     public String reverseWords(String s) {
+        char[] ch = s.toCharArray();
+        int left = 0;
+        for (int i = 0; i < ch.length; i++) {
+            if (s.charAt(i) == ' ') {
+                reverseChars(ch, left, i-1);
+                left = i + 1;
+            }
+        }
+        reverseChars(ch, left, s.length() - 1);
+        return new String(ch);
+    }
+
+    private void reverseChars(char[] ch, int from, int end) {
+        char temp;
+        while (from < end) {
+            temp = ch[from];
+            ch[from++] = ch[end];
+            ch[end--] = temp;
+        }
+    }
+
+
+
+    public String reverseWords2(String s) {
         StringBuilder stb = new StringBuilder(s.length());
         int spaceIndex = 0;
         for (int i = 0; i < s.length(); ) {
