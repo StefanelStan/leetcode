dc95182c503141e28a168a27c5e399d77339e262
StefanelStan <stefanel.stan@yahoo.com>
2023-12-15 20:27:41 +0000

    LeetCode Dec 15 (2023 Annual Badge)

--- src/main/java/com/ss/leetcode/LC2021/march/DestinationCity.java
+++ src/main/java/com/ss/leetcode/LC2021/march/DestinationCity.java
@@ -1,12 +1,29 @@
 package com.ss.leetcode.LC2021.march;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class DestinationCity {
     // https://leetcode.com/problems/destination-city/
     public String destCity(List<List<String>> paths) {
+        Set<String> from = new HashSet<>();
+        Set<String> to = new HashSet<>();
+        for (List<String> path : paths) {
+            from.add(path.get(0));
+            to.add(path.get(1));
+        }
+        for (String destination : to) {
+            if (!from.contains(destination)) {
+                return destination;
+            }
+        }
+        return "";
+    }
+
+    public String destCity2(List<List<String>> paths) {
         final Map<String, Integer> cityMap = new HashMap<>();
         paths.forEach(path -> {
             cityMap.merge(path.get(0), 2, Integer::sum);
--- src/main/java/com/ss/leetcode/LC2021/september/VerticalOrderTraversalOfABinaryTree.java
+++ src/main/java/com/ss/leetcode/LC2021/september/VerticalOrderTraversalOfABinaryTree.java
@@ -3,6 +3,7 @@ package com.ss.leetcode.LC2021.september;
 import com.ss.leetcode.shared.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -11,6 +12,50 @@ import java.util.stream.Collectors;
 public class VerticalOrderTraversalOfABinaryTree {
     // https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<TraversalNode> traversalNodes = new ArrayList<>();
+        traverseTree(root, 0, 0, traversalNodes);
+        return getVerticalTraversal(traversalNodes);
+    }
+
+    private List<List<Integer>> getVerticalTraversal(List<TraversalNode> traversalNodes) {
+        traversalNodes.sort(Comparator.comparingInt(TraversalNode::getCol).thenComparingInt(c -> c.row).thenComparingInt(c -> c.value));
+        List<List<Integer>> verticalTraversal = new ArrayList<>();
+        List<Integer> first = new ArrayList<>();
+        first.add(traversalNodes.get(0).value);
+        verticalTraversal.add(first);
+        for (int i = 1; i < traversalNodes.size(); i++) {
+            if (traversalNodes.get(i).col != traversalNodes.get(i - 1).col) {
+                verticalTraversal.add(new ArrayList<>());
+            }
+            verticalTraversal.get(verticalTraversal.size() -1).add(traversalNodes.get(i).value);
+        }
+        return verticalTraversal;
+    }
+
+    private void traverseTree(TreeNode node, int col, int row, List<TraversalNode> traversalNodes) {
+        if (node != null) {
+            traversalNodes.add(new TraversalNode(col, row, node.val));
+            traverseTree(node.left, col - 1, row + 1, traversalNodes);
+            traverseTree(node.right, col + 1, row + 1, traversalNodes);
+        }
+    }
+
+    private static class TraversalNode {
+        int col;
+        int row;
+        int value;
+        public TraversalNode(int col, int row, int value) {
+            this.col = col;
+            this.row = row;
+            this.value = value;
+        }
+
+        public int getCol() {
+            return col;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal2(TreeNode root) {
         if (root.left == root.right) {
             return List.of(List.of(root.val));
         }
