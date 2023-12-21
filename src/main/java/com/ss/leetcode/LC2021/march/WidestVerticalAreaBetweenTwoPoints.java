24973adf1ce058efd857e26057acde72307fe031
StefanelStan <stefanel.stan@yahoo.com>
2023-12-21 16:11:14 +0000

    LeetCode Dec 21 (Holiday Start Day)

--- src/main/java/com/ss/leetcode/LC2021/march/WidestVerticalAreaBetweenTwoPoints.java
+++ src/main/java/com/ss/leetcode/LC2021/march/WidestVerticalAreaBetweenTwoPoints.java
@@ -1,12 +1,29 @@
 package com.ss.leetcode.LC2021.march;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class WidestVerticalAreaBetweenTwoPoints {
     // https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
+    // Faster to copy into a 1D array and sort than sort 2D array
     public int maxWidthOfVerticalArea(int[][] points) {
+        int[] newPoints = new int[points.length];
+        for (int i = 0; i < points.length; i++) {
+            newPoints[i] = points[i][0];
+        }
+        Arrays.sort(newPoints);
+        int maxWidth = 0;
+        for (int i = 1; i < points.length; i++) {
+            maxWidth = Math.max(maxWidth, newPoints[i] - newPoints[i-1]);
+        }
+        return maxWidth;
+    }
+
+
+
+    public int maxWidthOfVerticalArea2(int[][] points) {
         List<Integer> width = new ArrayList<>(points.length);
         for (int[] point : points) {
             if (point[1] > 0) {
