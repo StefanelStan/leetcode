6006f07d3bb1c82fcda69ff53128559319861a46
StefanelStan <stefanel.stan@yahoo.com>
2024-01-01 22:04:44 +0200

    LeetCode Jan 01 (Noul An 2024 & Assign Cookies)

--- src/main/java/com/ss/leetcode/LC2021/november/AssignCookies.java
+++ src/main/java/com/ss/leetcode/LC2021/november/AssignCookies.java
@@ -4,17 +4,24 @@ import java.util.Arrays;
 
 public class AssignCookies {
     // https://leetcode.com/problems/assign-cookies/
+    /** Algorithm
+        1. Sort the greed and sizes
+        2. With two indices child and cookie both starting from 0
+        3. While child < g.length and cookie < s.length
+            - If you can satisfy the current greed (g[child] <= s[cookie]), then assign the cookie to current child and move on
+            - Else (the size is too small for current child) move to next cookie size and see if this size suits the current child.
+        4. Return the index of last child.
+     */
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int lastContent = s.length - 1;
-        int content = 0;
-        for (int child = g.length - 1; child >= 0 && lastContent >= 0; child--) {
-            if (s[lastContent] >= g[child]) {
-                content++;
-                lastContent--;
+        int child = 0, cookie = 0;
+        while (child < g.length && cookie < s.length) {
+            if (g[child] <= s[cookie]) {
+                child++;
             }
+            cookie++;
         }
-        return content;
+        return child;
     }
 }
