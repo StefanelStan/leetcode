df8405ae8e667ff9eb4e464947a79db025f94e00
StefanelStan <stefanel.stan@yahoo.com>
2024-01-30 21:10:45 +0000

    LeetCode Jan 30 (Reverse Polish Notation - Stack)

--- src/main/java/com/ss/leetcode/LC2022/april/EvaluateReversePolishNotation.java
+++ src/main/java/com/ss/leetcode/LC2022/april/EvaluateReversePolishNotation.java
@@ -2,10 +2,37 @@ package com.ss.leetcode.LC2022.april;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class EvaluateReversePolishNotation {
     // https://leetcode.com/problems/evaluate-reverse-polish-notation/
+    /** Algorithm
+        1. Use a Stack (can or Dequeue) to stack the numbers
+        2. If current token is an operand ( < '0'), then pop the last two nums, apply operation and add the result
+        3. Return the result from the stack.
+     */
     public int evalRPN(String[] tokens) {
+        Stack<Integer> lastNums = new Stack<>();
+        for (String token : tokens) {
+            if (token.length() == 1 && token.charAt(0) < '0') {
+                lastNums.push(operate(token.charAt(0), lastNums.pop(), lastNums.pop()));
+            } else {
+                lastNums.push(Integer.valueOf(token));
+            }
+        }
+        return lastNums.pop();
+    }
+
+    private int operate(char op, int b, int a) {
+        return switch(op) {
+            case '-' -> a - b;
+            case '+' -> a + b;
+            case '/' -> a / b;
+            default -> a * b;
+        };
+    }
+
+    public int evalRPN2(String[] tokens) {
         Deque<Integer> numbers = new LinkedList<>();
         for (String token : tokens) {
             switch (token) {
