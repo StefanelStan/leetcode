f01e8e9842c506811b9d0d6fc7cbfcd834899da8
StefanelStan <stefanel.stan@yahoo.com>
2024-04-14 23:37:54 +0100

    LeetCode Apr 14 (Sum of Left Leaves)

--- src/main/java/com/ss/leetcode/LC2021/august/SumOfLeftLeaves.java
+++ src/main/java/com/ss/leetcode/LC2021/august/SumOfLeftLeaves.java
@@ -4,7 +4,28 @@ import com.ss.leetcode.shared.TreeNode;
 
 public class SumOfLeftLeaves {
     // https://leetcode.com/problems/sum-of-left-leaves/
+    /** Algorithm
+        1. Create a function traverse(Node, isLeft) that takes a node and a flag isLeft
+        2. If node is null, return 0
+        3. If node if leaf, return according to flag (node.val for left and 0 for right)
+        4. Traverse tree and return the sum of traverse(node.left, true) + traverse(node.right, false)
+     */
     public int sumOfLeftLeaves(TreeNode root) {
+        return traverseTree(root, false);
+    }
+
+    private int traverseTree(TreeNode node, boolean isLeft) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == node.right) {
+            return isLeft ? node.val : 0;
+        }
+        return traverseTree(node.left, true) + traverseTree(node.right, false);
+    }
+
+
+    public int sumOfLeftLeaves2(TreeNode root) {
         if (root == null) {
             return 0;
         }
@@ -22,7 +43,7 @@ public class SumOfLeftLeaves {
         return sum;
     }
 
-    public int sumOfLeftLeaves2(TreeNode root) {
+    public int sumOfLeftLeaves3(TreeNode root) {
         int[] sumOfLeftLeaves = new int[]{0};
         getSumOfLeftLeaves(root, root, sumOfLeftLeaves);
         return sumOfLeftLeaves[0];
