130153637cb1af684ca98dee2f603c65e0e6c2c2
StefanelStan <stefanel.stan@yahoo.com>
2024-05-01 21:17:44 +0100

    LeetCode May 01 (Reverse Prefix Of Word)

--- src/main/java/com/ss/leetcode/LC2021/october/ReversePrefixOfWord.java
+++ src/main/java/com/ss/leetcode/LC2021/october/ReversePrefixOfWord.java
@@ -3,6 +3,19 @@ package com.ss.leetcode.LC2021.october;
 public class ReversePrefixOfWord {
     // https://leetcode.com/problems/reverse-prefix-of-word/
     public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if (index == -1) {
+            return word;
+        }
+        StringBuilder stb = new StringBuilder(word.length());
+        for (int from = index; from >= 0; from--) {
+            stb.append(word.charAt(from));
+        }
+        stb.append(word, index + 1, word.length());
+        return stb.toString();
+    }
+
+    public String reversePrefix2(String word, char ch) {
         StringBuilder stb = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             if (word.charAt(i) == ch) {
