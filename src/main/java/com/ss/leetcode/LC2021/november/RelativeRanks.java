d36a20587a2a398bc980fa4d7be10cbf7f36752b
StefanelStan <stefanel.stan@yahoo.com>
2024-05-08 20:53:06 +0300

    LeetCode May 08 (Relative Ranks - sort based on score[a])

--- src/main/java/com/ss/leetcode/LC2021/november/RelativeRanks.java
+++ src/main/java/com/ss/leetcode/LC2021/november/RelativeRanks.java
@@ -1,11 +1,38 @@
 package com.ss.leetcode.LC2021.november;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class RelativeRanks {
     // https://leetcode.com/problems/relative-ranks/
     public String[] findRelativeRanks(int[] score) {
+        List<Integer> positions = new ArrayList<>(score.length);
+        for (int i = 0; i < score.length; i++) {
+            positions.add(i);
+        }
+        Collections.sort(positions, Comparator.comparingInt(a -> score[a]));
+        String[] ranks = new String[score.length];
+        for (int i = score.length - 1, rank = 0; i >= 0; i--, rank++) {
+            ranks[positions.get(i)] = getRank(rank);
+        }
+        return ranks;
+    }
+
+    private String getRank(int pos) {
+        return switch(pos) {
+            case 0 -> "Gold Medal";
+            case 1 -> "Silver Medal";
+            case 2 -> "Bronze Medal";
+            default -> String.valueOf(pos + 1);
+        };
+    }
+
+
+
+    public String[] findRelativeRanks2(int[] score) {
         if (score.length == 1) {
             return new String[]{"Gold Medal"};
         }
