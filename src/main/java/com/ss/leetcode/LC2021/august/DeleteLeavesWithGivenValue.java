b9fe5ded59f6214eb6166d22b7cd448795bcdfd6
StefanelStan <stefanel.stan@yahoo.com>
2024-05-17 21:51:26 +0100

    LeetCode May 17 (Delete Leaves With Given Value - DFS InOrder - PRO MODE REDO)

--- src/main/java/com/ss/leetcode/LC2021/august/DeleteLeavesWithGivenValue.java
+++ src/main/java/com/ss/leetcode/LC2021/august/DeleteLeavesWithGivenValue.java
@@ -4,7 +4,39 @@ import com.ss.leetcode.shared.TreeNode;
 
 public class DeleteLeavesWithGivenValue {
     // https://leetcode.com/problems/delete-leaves-with-a-given-value/
+    /** Algorithm
+        1. Use PreOrder DFS in a recursive function that takes the current node and parent node
+        2. If current node is leaf and has the target value, set parent.left/right to null
+            (depends on current node is left or right child)
+        3. Upon current node, when left/right subtree have been processed, if current node has become leaf,
+            it will get deleted as well from its parent
+        4. If root itself has become a leaf with target value, return null.
+     */
     public TreeNode removeLeafNodes(TreeNode root, int target) {
+        traverseAndDelete(root, null, target);
+        return root.left == root.right && root.val == target ? null : root;
+    }
+
+    private void traverseAndDelete(TreeNode node, TreeNode parent, int target) {
+        if (node == null) {
+            return;
+        }
+        traverseAndDelete(node.left, node, target);
+        traverseAndDelete(node.right, node, target);
+        if (node.left == node.right && node.val == target) {
+            if (parent != null) {
+                if (parent.left == node) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+        }
+    }
+
+
+
+    public TreeNode removeLeafNodes2(TreeNode root, int target) {
         TreeNode myRoot = new TreeNode(1, root, null);
         needsTrimming(root, myRoot, true, target);
         return myRoot.left;
