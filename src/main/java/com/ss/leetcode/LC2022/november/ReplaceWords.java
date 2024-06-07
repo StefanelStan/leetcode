f3d528c26cf173b5d46251fd86cf26abd6d64726
StefanelStan <stefanel.stan@yahoo.com>
2024-06-07 22:46:59 +0100

    LeetCode Jun 07 (Replace Words - Trie REDO - 7ms beats 100%)

--- src/main/java/com/ss/leetcode/LC2022/november/ReplaceWords.java
+++ src/main/java/com/ss/leetcode/LC2022/november/ReplaceWords.java
@@ -4,7 +4,80 @@ import java.util.List;
 
 public class ReplaceWords {
     // https://leetcode.com/problems/replace-words/
-    public String replaceWords(List<String> dictionary, String sentence2) {
+    /** Algorithm
+     1. Split the sentence into chunks by " " (space delimiter)
+     2. For each chunk (word) apart from last one you will append it's replacement and " ".
+     For last word, you will just append the replacement.
+     3. Create a Trie for dictionary
+     4. For each word, walk on the trie. If you encounter a node that is word, return the substring (0, index+1)
+     5. If you step on a null node OR you reach the end of word, then append the whole word as it is.
+     */
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String[] chunks = sentence.split(" ");
+        TrieNode root = buildTrie(dictionary);
+        StringBuilder stb = new StringBuilder();
+        for (int i = 0; i < chunks.length - 1; i++) {
+            stb.append(findReplacement(chunks[i], root));
+            stb.append(' ');
+        }
+        stb.append(findReplacement(chunks[chunks.length - 1], root));
+        return stb.toString();
+    }
+
+    private String findReplacement(String word, TrieNode root) {
+        int index = 0, codePoint;
+        for (; index < word.length(); index++) {
+            codePoint = word.charAt(index) - 'a';
+            root = root.getNode(codePoint);
+            if (root == null) {
+                index = word.length();
+            } else if (root.isWord) {
+                index++;
+                break;
+            }
+        }
+        return index >= word.length() ? word : word.substring(0, index);
+    }
+
+    private TrieNode buildTrie(List<String> dictionary) {
+        TrieNode root = new TrieNode();
+        for (String word : dictionary) {
+            addToTrie(word, root);
+        }
+        return root;
+    }
+
+    private void addToTrie(String word, TrieNode root) {
+        int codePoint;
+        for (int i = 0; i < word.length(); i++) {
+            codePoint = word.charAt(i) - 'a';
+            root = root.addNode(codePoint);
+        }
+        root.isWord = true;
+    }
+
+    private static class TrieNode {
+        TrieNode[] nodes;
+        boolean isWord;
+        public TrieNode() {
+            nodes = new TrieNode[26];
+        }
+
+        private TrieNode addNode(int codePoint) {
+            if (nodes[codePoint] == null) {
+                nodes[codePoint] = new TrieNode();
+            }
+            return nodes[codePoint];
+        }
+
+        private TrieNode getNode(int codePoint) {
+            return nodes[codePoint];
+        }
+    }
+
+
+
+    public String replaceWords2(List<String> dictionary, String sentence2) {
         char[] sentence = sentence2.toCharArray();
         StringBuilder replacedWord = new StringBuilder(1000);
         Trie root = buildDictionary(dictionary);
