deede26a5f1d14a77c44d8dd02e54909049a592f
StefanelStan <stefanel.stan@yahoo.com>
2024-06-10 23:50:05 +0100

    LeetCode Jun 10 (Height Checker - Count Bucket Sort)

--- src/main/java/com/ss/leetcode/LC2021/july/HeightChecker.java
+++ src/main/java/com/ss/leetcode/LC2021/july/HeightChecker.java
@@ -4,7 +4,30 @@ import java.util.Arrays;
 
 public class HeightChecker {
     // https://leetcode.com/problems/height-checker/
+    /** Algorithm
+        1. Count the nums into an int[] count of size 101
+        2. For each height, traverse count while you reach a count > 0.
+        3. If count[index] == height, then you are on a good height index, else on a bad height/mismatch
+        4. Every time, decrease the count[index] by 1 to simulate a move to the right of the count
+     */
     public int heightChecker(int[] heights) {
+        int mismatches = 0;
+        int[] count = new int[101];
+        for (int height : heights) {
+            count[height]++;
+        }
+        int countIndex = 0;
+        for (int height : heights) {
+            while (count[countIndex] == 0) {
+                countIndex++;
+            }
+            mismatches += height == countIndex ? 0 : 1;
+            count[countIndex]--;
+        }
+        return mismatches;
+    }
+
+    public int heightChecker2(int[] heights) {
         int[] ordered = new int[heights.length];
         System.arraycopy(heights, 0, ordered, 0, heights.length);
         Arrays.sort(ordered);
