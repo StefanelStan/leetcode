3513356cc48205948c5ecf7a161373a2c297b56a
StefanelStan <stefanel.stan@yahoo.com>
2024-06-12 23:30:15 +0100

    LeetCode Jun 12 (Sort Colors : 2 pointers or counting[3])

--- src/main/java/com/ss/leetcode/LC2021/january/SortColors.java
+++ src/main/java/com/ss/leetcode/LC2021/january/SortColors.java
@@ -6,17 +6,26 @@ import java.util.TreeMap;
 public class SortColors {
     // https://leetcode.com/problems/sort-colors/
     public void sortColors(int[] nums) {
-        Map<Integer, Integer> numberCount = new TreeMap<>();
-        for(int num: nums) {
-            numberCount.merge(num, 1, Integer::sum);
-        }
-        final int[] startingIndex = {0};
-        numberCount.forEach((key, value) -> {
-            for(int i = startingIndex[0]; i < startingIndex[0] + value; i++) {
-                nums[i] = key;
+        int start = sortColor(nums, 0, 0);
+        sortColor(nums, start, 1);
+    }
+
+    private int sortColor(int[] nums, int from, int color) {
+        for (int end = nums.length -1; from <= end;) {
+            while (from <= end && nums[from] == color) {
+                from++;
             }
-            startingIndex[0] += value;
-        });
+            while (from <= end && nums[end] != color) {
+                end--;
+            }
+            if (from < end) {
+                nums[end] = nums[from];
+                nums[from] = color;
+                from++;
+                end--;
+            }
+        }
+        return from;
     }
 
     public void sortColors2(int[] nums) {
@@ -32,4 +41,19 @@ public class SortColors {
             pos += color[i];
         }
     }
+
+    public void sortColors3(int[] nums) {
+        Map<Integer, Integer> numberCount = new TreeMap<>();
+        for(int num: nums) {
+            numberCount.merge(num, 1, Integer::sum);
+        }
+        final int[] startingIndex = {0};
+        numberCount.forEach((key, value) -> {
+            for(int i = startingIndex[0]; i < startingIndex[0] + value; i++) {
+                nums[i] = key;
+            }
+            startingIndex[0] += value;
+        });
+    }
+
 }
