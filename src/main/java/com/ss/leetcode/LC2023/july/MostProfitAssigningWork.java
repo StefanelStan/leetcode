e7c744ead0d6921a55ec1bb586b662938110c074
StefanelStan <stefanel.stan@yahoo.com>
2024-06-21 06:51:33 +0300

    LeetCode Jun 18 (ID Pic & Most Profit Assigning Work)

--- src/main/java/com/ss/leetcode/LC2023/july/MostProfitAssigningWork.java
+++ src/main/java/com/ss/leetcode/LC2023/july/MostProfitAssigningWork.java
@@ -5,6 +5,33 @@ import java.util.Comparator;
 
 public class MostProfitAssigningWork {
     // https://leetcode.com/problems/most-profit-assigning-work
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int[][] jobsByDifficulty = getJobsByDifficulty(difficulty, profit);
+        Arrays.sort(worker);
+        int maxProfit = 0;
+        int jobIndex = 0;
+        for (int w : worker) {
+            while (jobIndex < difficulty.length && w >= jobsByDifficulty[jobIndex][0]) {
+                jobIndex++;
+            }
+            maxProfit += (jobIndex == 0 ? 0 : jobsByDifficulty[jobIndex - 1][1]);
+        }
+        return maxProfit;
+    }
+
+    private int[][] getJobsByDifficulty(int[] difficulty, int[] profit) {
+        int[][] jobsByDifficulty = new int[difficulty.length][2];
+        for (int i = 0; i < jobsByDifficulty.length; i++) {
+            jobsByDifficulty[i] = new int[]{difficulty[i], profit[i]};
+        }
+        Arrays.sort(jobsByDifficulty, Comparator.comparingInt(j -> j[0]));
+        for (int i = 1; i < difficulty.length; i++) {
+            jobsByDifficulty[i][1] = Math.max(jobsByDifficulty[i-1][1], jobsByDifficulty[i][1]);
+        }
+        return jobsByDifficulty;
+    }
+
+
     /** Algorithm
         1. Create an array[n][2] jobs [difficulty,profit] and sort it by difficulty
         2. Traverse this sorted array and set the max profit/ rolling profit from easiest job to hardest job. jobs[i][1]
@@ -13,9 +40,9 @@ public class MostProfitAssigningWork {
             as the last position will have the highest proffit guaranteed (step #2 does this)
         4. Return the sum.
      */
-    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+    public int maxProfitAssignment2(int[] difficulty, int[] profit, int[] worker) {
         int maxProfit = 0, maxJobIndex;
-        int[][] jobsByDifficulty = getJobsByDifficulty(difficulty, profit);
+        int[][] jobsByDifficulty = getJobsByDifficulty2(difficulty, profit);
         for (int w : worker) {
             maxJobIndex = findMaxJobIndex(jobsByDifficulty, w);
             maxProfit += maxJobIndex != -1 ? jobsByDifficulty[maxJobIndex][1] : 0;
@@ -23,7 +50,7 @@ public class MostProfitAssigningWork {
         return maxProfit;
     }
 
-    private int[][] getJobsByDifficulty(int[] difficulty, int[] profit) {
+    private int[][] getJobsByDifficulty2(int[] difficulty, int[] profit) {
         int[][] jobsByDifficulty = new int[difficulty.length][2];
         for(int i = 0; i < difficulty.length; i++) {
             jobsByDifficulty[i][0] = difficulty[i];
