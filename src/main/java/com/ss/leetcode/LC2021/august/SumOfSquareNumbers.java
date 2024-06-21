d6efb8a09c21bedafd37045f6a00949a2a20f42e
StefanelStan <stefanel.stan@yahoo.com>
2024-06-21 06:48:16 +0300

    LeetCode Jun 17 (Digi C Sign & Sum of Square Numbers BS between 0 and sqrt(n))

--- src/main/java/com/ss/leetcode/LC2021/august/SumOfSquareNumbers.java
+++ src/main/java/com/ss/leetcode/LC2021/august/SumOfSquareNumbers.java
@@ -3,6 +3,21 @@ package com.ss.leetcode.LC2021.august;
 public class SumOfSquareNumbers {
     // https://leetcode.com/problems/sum-of-square-numbers/
     public boolean judgeSquareSum(int c) {
+        int left = 0, right = (int)Math.sqrt(c), squareSum = 0;
+        while (left <= right) {
+            squareSum = (left * left) + (right * right);
+            if (squareSum == c) {
+                return true;
+            } else if (squareSum < 0 || squareSum > c) {
+                right --;
+            } else {
+                left++;
+            }
+        }
+        return false;
+    }
+
+    public boolean judgeSquareSum2(int c) {
         int low = 0;
         int high = (int)Math.sqrt(c);
         long tempSum;
