d5954e5af9f840768d8b690827a1e25a22e8b4d4
StefanelStan <stefanel.stan@yahoo.com>
2024-06-28 18:24:00 +0300

    LeetCode Jun 28 (Living Room + Sliding Door Moving & Max Total Importance Of Roads - indegree, sort and backwards)

--- src/main/java/com/ss/leetcode/LC2022/may/MaximumTotalImportanceOfRoads.java
+++ src/main/java/com/ss/leetcode/LC2022/may/MaximumTotalImportanceOfRoads.java
@@ -5,7 +5,28 @@ import java.util.Comparator;
 
 public class MaximumTotalImportanceOfRoads {
     // https://leetcode.com/problems/maximum-total-importance-of-roads/
+    /** Algorithm
+        1. Use an int[] to mark the indegree/outdegree of each city: [a,b] -> cityImportance[a]++ and cityImportance[b]++;
+        2. Sort ASC the cityImportance. Each indegree means there is an edge coming in/out from that city, so it's
+            importance will be multiplied with its indegree.
+        3. Starting from the back, multiply and add each cityImportance with it's index. The index will be the unique value (n to 1)
+     */
     public long maximumImportance(int n, int[][] roads) {
+        long maxImportance = 0;
+        int[] cityImportance = new int[n];
+        for (int[] road : roads) {
+            cityImportance[road[0]]++;
+            cityImportance[road[1]]++;
+        }
+        Arrays.sort(cityImportance);
+        for (int i = n -1; i >= 0; i--) {
+            maxImportance += ((long)(i + 1) * cityImportance[i]);
+        }
+        return maxImportance;
+    }
+
+    // INEFFICIENT
+    public long maximumImportance2(int n, int[][] roads) {
         int[][] cities = new int[n][3];
         for(int i = 0; i < cities.length; i++) {
             cities[i][0] = i;
