6143f69bd4e7b4de870a66b3d021a44e67b8c858
StefanelStan <stefanel.stan@yahoo.com>
2024-07-01 21:54:54 +0300

    LeetCode Jul 01 (Plumming Work Day 7 - 2 x Baterii & 3 Consec Odds)

--- src/main/java/com/ss/leetcode/LC2021/september/ThreeConsecutiveOdds.java
+++ src/main/java/com/ss/leetcode/LC2021/september/ThreeConsecutiveOdds.java
@@ -2,6 +2,11 @@ package com.ss.leetcode.LC2021.september;
 
 public class ThreeConsecutiveOdds {
     // https://leetcode.com/problems/three-consecutive-odds/
+    /** Algorithm
+        1.Traverse with i from 0 to n-2
+        2. If arr[i] %2  == 1 && arr[i+1] % 2 ==1 && arr[i+2] % 2 == 1 return true
+        3. Return false by default
+     */
     public boolean threeConsecutiveOdds(int[] arr) {
         for (int i = 0; i < arr.length - 2; i++) {
             if (arr[i] % 2 == 1 && arr[i+1] % 2 == 1 && arr[i+2] % 2== 1) {
