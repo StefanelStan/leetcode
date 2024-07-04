d7b713d5994b1150aa57b07b85b3105b558512f5
StefanelStan <stefanel.stan@yahoo.com>
2024-07-04 19:43:21 +0300

    LeetCode Jul 04 (New PVC Doors & Merge Nodes In Between Zeroes - traverse and add vals)

--- src/main/java/com/ss/leetcode/LC2022/february/MergeNodesInBetweenZeros.java
+++ src/main/java/com/ss/leetcode/LC2022/february/MergeNodesInBetweenZeros.java
@@ -4,7 +4,37 @@ import com.ss.leetcode.shared.ListNode;
 
 public class MergeNodesInBetweenZeros {
     // https://leetcode.com/problems/merge-nodes-in-between-zeros/
+    /** Algorithm
+        1. Traverse the list and add values of nodes after each zero.
+        2. When you encounter a zero, insert a new node into your new list. Append the node to the tail.
+        3. Return the new head.
+     */
     public ListNode mergeNodes(ListNode head) {
+        head = head.next;
+        int sum = 0;
+        ListNode newHead = null, tail = null;
+        while (head != null) {
+            if (head.val == 0) {
+                if (sum > 0) {
+                    ListNode newNode = new ListNode(sum);
+                    if (newHead == null) {
+                        newHead = tail = newNode;
+                    } else {
+                        tail.next = newNode;
+                        tail = newNode;
+                    }
+                }
+                sum = 0;
+            } else {
+                sum += head.val;
+            }
+            head = head.next;
+        }
+        return newHead;
+    }
+
+
+    public ListNode mergeNodes2(ListNode head) {
         ListNode preHead = new ListNode(0);
         ListNode currentNewNode = null;
         int sum = 0;
