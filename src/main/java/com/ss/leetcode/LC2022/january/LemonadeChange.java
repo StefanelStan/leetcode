b20640b2945e2260622cb9f1e19db034c8a19611
StefanelStan <stefanel.stan@yahoo.com>
2024-08-15 21:48:19 +1000

    LeetCode Aug 15 (Lemonade Exchange)

--- src/main/java/com/ss/leetcode/LC2022/january/LemonadeChange.java
+++ src/main/java/com/ss/leetcode/LC2022/january/LemonadeChange.java
@@ -3,6 +3,22 @@ package com.ss.leetcode.LC2022.january;
 public class LemonadeChange {
     // https://leetcode.com/problems/lemonade-change/
     public boolean lemonadeChange(int[] bills) {
+        int[] coins = {0, 0, 0};
+        int restToGive;
+        for (int i = 0; i < bills.length && coins[0] >= 0; i++) {
+            coins[bills[i] / 10]++;
+            restToGive = bills[i] - 5;
+            if (restToGive == 15 && coins[1] > 0) {
+                coins[1]--;
+                restToGive -= 10;
+            }
+            coins[0] -= restToGive / 5;
+        }
+        return coins[0] >= 0;
+    }
+
+
+    public boolean lemonadeChange2(int[] bills) {
         int fives = 0, tens = 0;
         int change;
         for (int bill : bills) {
