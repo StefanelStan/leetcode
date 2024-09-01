b96fe2c621640fcd5706826a9c7610fcd363bc7c
StefanelStan <stefanel.stan@yahoo.com>
2024-09-01 23:03:06 +1000

    LeetCode Sep 01 (SD Day)

--- src/main/java/com/ss/leetcode/LC2021/october/Convert1DArrayInto2DArray.java
+++ src/main/java/com/ss/leetcode/LC2021/october/Convert1DArrayInto2DArray.java
@@ -2,7 +2,25 @@ package com.ss.leetcode.LC2021.october;
 
 public class Convert1DArrayInto2DArray {
     // https://leetcode.com/contest/biweekly-contest-62/problems/convert-1d-array-into-2d-array/
+    /** Algorithm
+        1. Fail fast: if n * m != original.length return int[0][0]
+        2. Else, allocate a new matrix[m][n]
+        3. Iterate with index i over original. i / n will give you the row number to insert and i % n the column index
+        4. Insert elements and return answer.
+     */
     public int[][] construct2DArray(int[] original, int m, int n) {
+        if (n * m != original.length) {
+            return new int[0][0];
+        }
+        int[][] answer = new int[m][n];
+        for (int i = 0; i < original.length; i++) {
+            answer[i / n][i % n] = original[i];
+        }
+        return answer;
+    }
+
+
+    public int[][] construct2DArray2(int[] original, int m, int n) {
         if (m * n != original.length) {
             return new int[0][0];
         }
