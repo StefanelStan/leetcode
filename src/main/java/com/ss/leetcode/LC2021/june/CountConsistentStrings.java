e10a470f3d5c290b4b46a2837e5e00e9b1f7c8f8
StefanelStan <stefanel.stan@yahoo.com>
2024-09-12 22:14:39 +1000

    LeetCode Sep 12 (Reasons Fail System Design Course - Taro)

--- src/main/java/com/ss/leetcode/LC2021/june/CountConsistentStrings.java
+++ src/main/java/com/ss/leetcode/LC2021/june/CountConsistentStrings.java
@@ -6,6 +6,24 @@ import java.util.TreeSet;
 public class CountConsistentStrings {
     // https://leetcode.com/problems/count-the-number-of-consistent-strings/
     public int countConsistentStrings(String allowed, String[] words) {
+        boolean[] present = new boolean[26];
+        int count = 0;
+        for (int i = 0; i < allowed.length(); i++) {
+            present[allowed.charAt(i) - 'a'] = true;
+        }
+        boolean isConsistent;
+        for (String word : words) {
+            isConsistent = true;
+            for (int i = 0; i < word.length() && isConsistent; i++) {
+                isConsistent = present[word.charAt(i) - 'a'];
+            }
+            count += isConsistent ? 1 : 0;
+        }
+        return count;
+    }
+
+
+    public int countConsistentStrings2(String allowed, String[] words) {
         Set<Integer> allowedChars = new TreeSet<>();
         int consistentStrings = 0;
         for (int i = 0; i < allowed.length(); i++) {
