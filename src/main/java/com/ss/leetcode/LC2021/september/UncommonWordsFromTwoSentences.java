aab77c522af47e86f97b539309b725f839e2a5dc
StefanelStan <stefanel.stan@yahoo.com>
2024-09-17 20:23:06 +1000

    LeetCode Sep 17 (Uncommon Words From Two Sentences)

--- src/main/java/com/ss/leetcode/LC2021/september/UncommonWordsFromTwoSentences.java
+++ src/main/java/com/ss/leetcode/LC2021/september/UncommonWordsFromTwoSentences.java
@@ -7,7 +7,38 @@ import java.util.Map;
 
 public class UncommonWordsFromTwoSentences {
     // https://leetcode.com/problems/uncommon-words-from-two-sentences/
+    /** Algorithm
+        1. Get the words of each sentence (s1.split(" "));
+        2. Use a single Map<String, Integer> to count the words of each sentence. If a word is unique, then its count will be 1
+        3. Traverse the map and if the count is one, add it to answer.
+     */
     public String[] uncommonFromSentences(String s1, String s2) {
+        final List<String> uncommon = new ArrayList<>();
+        Map<String, Integer> wordCount = new HashMap<>();
+        countWords(s1, wordCount);
+        countWords(s2, wordCount);
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                uncommon.add(entry.getKey());
+            }
+        }
+        String[] answer = new String[uncommon.size()];
+        int insertIndex = 0;
+        for (String word : uncommon) {
+            answer[insertIndex++] = word;
+        }
+        return answer;
+    }
+
+    private void countWords(String sentence, Map<String, Integer> count) {
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            count.merge(word, 1, Integer::sum);
+        }
+    }
+
+
+    public String[] uncommonFromSentences2(String s1, String s2) {
         Map<String, Integer> s1Words = mapWords(s1);
         Map<String, Integer> s2words = mapWords(s2);
         List<String> diffWords = new ArrayList<>();
