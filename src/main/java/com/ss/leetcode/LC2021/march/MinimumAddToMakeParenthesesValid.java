ada05c4f1083a18e1e26070eee20481bb5f37fc4
StefanelStan <stefanel.stan@yahoo.com>
2024-10-09 22:56:02 +1100

    LeetCode Oct 09 (Min Add To Make Parths Valid - keep track of open/close)

--- src/main/java/com/ss/leetcode/LC2021/march/MinimumAddToMakeParenthesesValid.java
+++ src/main/java/com/ss/leetcode/LC2021/march/MinimumAddToMakeParenthesesValid.java
@@ -1,6 +1,13 @@
 package com.ss.leetcode.LC2021.march;
 
 public class MinimumAddToMakeParenthesesValid {
+    // https://leetcode.com/problems/minimum-add-to-make-parentheses-valid
+    /** Algorithm
+        1. Traverse s and keep track of opened and closed parenthesis
+        2. If current char is (, increment open
+        3. Else, if open > 0, decrement open, else increment closed.
+        4. Return openend + closed;
+     */
     public int minAddToMakeValid(String S) {
         int openingCount = 0, closingCount =0;
         for(int i = 0; i < S.length(); i++) {
