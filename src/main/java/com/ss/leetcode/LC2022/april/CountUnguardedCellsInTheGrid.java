8fcc0ca33cacdf12bdce0de14a2741442a7f2486
StefanelStan <stefanel.stan@yahoo.com>
2024-11-21 20:14:28 +1100

    LeetCode Nov 21 (Count Unguarded Cells Grid - Travel 4 dir and mark O/(W/G))

--- src/main/java/com/ss/leetcode/LC2022/april/CountUnguardedCellsInTheGrid.java
+++ src/main/java/com/ss/leetcode/LC2022/april/CountUnguardedCellsInTheGrid.java
@@ -4,6 +4,47 @@ public class CountUnguardedCellsInTheGrid {
     // https://leetcode.com/contest/biweekly-contest-77/problems/count-unguarded-cells-in-the-grid/
     private static final char WALL = 'W', GUARD = 'G', SEEN = 'S';
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+        int unguarded  = 0;
+        char[][] matrix = getMatrix(m, n, guards, walls);
+        for (int[] guard : guards) {
+            guardCells(matrix, guard[0], guard[1]);
+        }
+        for (char[] row : matrix) {
+            for (char cell : row) {
+                unguarded += (cell == 0 ? 1 : 0);
+            }
+        }
+        return unguarded;
+    }
+
+    private void guardCells(char[][] matrix, int i, int j) {
+        for (int c = j + 1; c < matrix[0].length && matrix[i][c] != WALL && matrix[i][c] != GUARD; c++) {
+            matrix[i][c] = SEEN;
+        }
+        for (int c = j - 1; c >= 0 && matrix[i][c] != WALL && matrix[i][c] != GUARD; c--) {
+            matrix[i][c] = SEEN;
+        }
+        for (int r = i - 1; r >= 0 && matrix[r][j] != WALL && matrix[r][j] != GUARD; r--) {
+            matrix[r][j] = SEEN;
+        }
+        for (int r = i + 1; r < matrix.length && matrix[r][j] != WALL && matrix[r][j] != GUARD; r++) {
+            matrix[r][j] = SEEN;
+        }
+    }
+
+    private char[][] getMatrix(int m, int n, int[][] guards, int[][] walls) {
+        char[][] matrix = new char[m][n];
+        for (int[] guard : guards) {
+            matrix[guard[0]][guard[1]] = GUARD;
+        }
+        for (int[] wall : walls) {
+            matrix[wall[0]][wall[1]] = WALL;
+        }
+        return matrix;
+    }
+
+
+    public int countUnguarded2(int m, int n, int[][] guards, int[][] walls) {
         char[][] cells = new char[m][n];
         for (int[] wall : walls) {
             cells[wall[0]][wall[1]] = WALL;
