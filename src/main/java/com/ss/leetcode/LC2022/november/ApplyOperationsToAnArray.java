7cb756968bc2673721237c79c51760025d31af06
StefanelStan <stefanel.stan@yahoo.com>
2025-03-01 08:11:25 +0200

    LeetCode Mar 01 (Apply Op To An Array - One pass)

--- src/main/java/com/ss/leetcode/LC2022/november/ApplyOperationsToAnArray.java
+++ src/main/java/com/ss/leetcode/LC2022/november/ApplyOperationsToAnArray.java
@@ -1,16 +1,38 @@
 package com.ss.leetcode.LC2022.november;
 
-import java.util.Arrays;
-
 public class ApplyOperationsToAnArray {
     // https://leetcode.com/problems/apply-operations-to-an-array/
+    /** Algorithm
+        1. Use an int[] applied to store the answer
+        2. Traverse nums and if current number is not zero
+            - insert into applied the double value of current number
+            - set next number to zero
+     */
+    public int[] applyOperations(int[] nums) {
+        int[] applied =  new int[nums.length];
+        int insertIndex = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != 0) {
+                if (nums[i] == nums[i + 1]) {
+                    applied[insertIndex++] = 2 * nums[i];
+                    nums[i + 1] = 0;
+                } else {
+                    applied[insertIndex++] = nums[i];
+                }
+            }
+        }
+        applied[insertIndex] = nums[nums.length - 1];
+        return applied;
+    }
+
+
     /** Algorithm
      *  1. Use an int[] answer to keep the answer: doubles and zeroes in order to avoid modifying the original array
      *  2. If i == i+1, set i = 2*i and i+1 to zero.
      *  3. Pay attention to last element.
      *  4. Swap the zeroes using 2 points: one for current i and one for last known zero/available position.
      */
-    public int[] applyOperations(int[] nums) {
+    public int[] applyOperations2(int[] nums) {
         int[] answer = multiplyAndZero(nums);
         shiftZeroes(answer);
         return answer;
