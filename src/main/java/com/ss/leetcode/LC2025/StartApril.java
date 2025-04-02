849a490f420a184239188c5a83f8eeda553fe8de
StefanelStan <stefanel.stan@yahoo.com>
2025-04-02 13:26:13 +0300

    LeetCode Apr 02 (Max Value Ordered Triple - maxOnLeft[] & maxOnRight)

---
+++ src/main/java/com/ss/leetcode/LC2025/StartApril.java
@@ -0,0 +1,20 @@
+package com.ss.leetcode.LC2025;
+
+import com.ss.leetcode.LC2025.april.MaximumValueOfAnOrderedTripletI;
+
+public class StartApril {
+    public static void main(String[] args) {
+        StartApril start = new StartApril();
+
+        start.maximumValueOfAnOrderedTripletI();
+    }
+
+    public void maximumValueOfAnOrderedTripletI() {
+        MaximumValueOfAnOrderedTripletI mvoaoti = new MaximumValueOfAnOrderedTripletI();
+
+        System.out.println("77 == " + mvoaoti.maximumTripletValue(new int[]{12,6,1,2,7}));
+        System.out.println("133 == " + mvoaoti.maximumTripletValue(new int[]{1,10,3,4,19}));
+        System.out.println("0 == " + mvoaoti.maximumTripletValue(new int[]{1,2,3}));
+        System.out.println("2345076549 == " + mvoaoti.maximumTripletValue(new int[]{23452, 1,2, 32256, 91345, 99999, 1, 10000}));
+    }
+}
