1bac40d292e8cdf35403ebc6d49248136fb634dc
StefanelStan <stefanel.stan@yahoo.com>
2025-09-11 14:48:19 +0300

    LeetCode Sep 11 (Sort Vowels String - FASTER COUNT SORT)

--- src/main/java/com/ss/leetcode/LC2023/november/SortVowelsInAString.java
+++ src/main/java/com/ss/leetcode/LC2023/november/SortVowelsInAString.java
@@ -5,6 +5,47 @@ import java.util.Arrays;
 public class SortVowelsInAString {
     // https://leetcode.com/problems/sort-vowels-in-a-string
     /** Algorithm
+     1. Use an int[z + 1] vowelCount to count how many vowels there are in s
+     2. Traverse S and if current char is a vowel, replace it with the first vowel from vowelCount. Keep track of current index of vowelCount
+     - Decrement the vowelCount by 1 after each replacement.
+     */
+    private static final int[] VOWELS = new int['z' + 1];
+    static {
+        VOWELS['a'] = VOWELS['e'] = VOWELS['i'] = VOWELS['o'] = VOWELS['u'] = 1;
+        VOWELS['A'] = VOWELS['E'] = VOWELS['I'] = VOWELS['O'] = VOWELS['U'] = 1;
+    }
+
+    public String sortVowels(String s) {
+        char[] sChars = s.toCharArray();
+        int[] vowelCount = getVowelCount(sChars);
+        int[] vowelIndex = {0};
+        for (int i = 0; i < sChars.length; i++) {
+            if (VOWELS[sChars[i]] > 0) {
+                sChars[i] = getNextVowel(vowelCount, vowelIndex);
+            }
+        }
+        return new String(sChars);
+    }
+
+    private char getNextVowel(int[] vowelCount, int[] index) {
+        while (vowelCount[index[0]] == 0) {
+            index[0]++;
+        }
+        vowelCount[index[0]]--;
+        return (char)index[0];
+
+    }
+
+    private int[] getVowelCount(char[] chars) {
+        int[] count = new int['z' + 1];
+        for (char c : chars) {
+            count[c] += VOWELS[c];
+        }
+        return count;
+    }
+
+
+    /** Algorithm
         1. You need to have the vowels into a sorted char[] vowels.
         2. Traverse s (sChars[]) and put the vowels into a StringBuilder. Get the chars[] vowels, sort and return the array.
         3. Traverse the sChars[]) and if current char is a vowel, replace it with the next available char from vowels[].
@@ -12,7 +53,7 @@ public class SortVowelsInAString {
         4. Return a new string made out of the modified sChars[].
      */
     private static boolean[] vowels;
-    public String sortVowels(String s) {
+    public String sortVowels2(String s) {
         initVowels();
         char[] chrs = s.toCharArray();
         char[] sortedVowels = sortVowels(chrs);
